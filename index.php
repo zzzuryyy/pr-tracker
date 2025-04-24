e1eb052a617b849455a5e926579b8bf95f6ba67a
zzzuryyy <perez.zuriel02@gmail.com>
2025-04-24 12:08:15 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+    <head>
+    </head>
+
+    <body>
+        <?php
+        //echo "hello php";
+        //include "sis_reg.php";
+        //include "sis_view.php";
+        include "login.php";
+        
+        ?>
+    </body>
+
+</html>
---
+++ logout.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+
+    </head>
+
+    <body>
+
+        <?php
+            header('location: login.php')
+
+        ?>
+    </body>
+
+</html>
